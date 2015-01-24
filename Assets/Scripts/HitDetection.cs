1c0b3d9dbe69bf0558bb1078c24147b9c5b6fede
Nick Pettit <nick@nickpettit.com>
2015-01-24 12:22:30 -0500

    Added hit detection

---
+++ Assets/Scripts/HitDetection.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HitDetection : MonoBehaviour {
+
+	public Text hitText;
+	public Color hitTextColor;
+	private Color clearColor;
+
+	void Awake () {
+		clearColor = new Color(hitTextColor.r, hitTextColor.g, hitTextColor.b, 0.0F);
+		hitText.color = clearColor;
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+
+		hitText.color = hitTextColor;
+	}
+
+	void Update() {
+
+		if (hitText.color != clearColor) {
+			hitText.color = Color.Lerp(hitText.color, clearColor, Time.deltaTime * 3f);
+		}
+
+	}
+
+}
