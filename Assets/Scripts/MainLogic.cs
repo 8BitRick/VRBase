3701ff54ba63621467fb6c984a8e07225fe1c771
Nick Pettit <nick@nickpettit.com>
2015-01-24 13:11:11 -0500

    Added a button to reset Oculus orientation

--- Assets/Scripts/MainLogic.cs
+++ Assets/Scripts/MainLogic.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class MainLogic : MonoBehaviour {
 
-	//Game start and restart logic goes here
+
+
+
+	void Update() {
+
+		// Game start and restart logic goes here
+
+		// Reset Oculus orientation
+		if (Input.GetKeyDown(KeyCode.Space))
+			OVRManager.display.RecenterPose();
+
+	}
 
 }
