0e5c32c3d73e8102259a01221440c4b611402079
Nick Pettit <nick@nickpettit.com>
2015-01-24 10:57:23 -0500

    Created the basic object spawner and launcher

--- Assets/Scripts/MainLogic.cs
+++ Assets/Scripts/MainLogic.cs
@@ -3,31 +3,6 @@ using System.Collections;
 
 public class MainLogic : MonoBehaviour {
 
-    public GameObject myLight;
+	//Game start and restart logic goes here
 
-	// Use this for initialization
-	void Start () {
-        for (int y = 0; y < 5; y++)
-        {
-            for (int x = 0; x < 5; x++)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.AddComponent<Rigidbody>();
-                cube.tag = "ENEMY";
-                cube.transform.position = new Vector3(x, y, 0);
-            }
-        }
-
-        myLight = new GameObject("My Light");
-        myLight.AddComponent<Light>();
-        myLight.light.color = Color.green;
-        myLight.transform.position = new Vector3(1, 1, -6);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        System.Converter<float, Vector3> posy = (newy) => new Vector3(myLight.transform.position.x, newy, myLight.transform.position.z);
-        myLight.transform.position = posy(Mathf.Sin(Time.time * 2) * 2 + 5.0f);
-        myLight.light.intensity = (0.5f + ((Mathf.Sin(Time.time * 2.0f)*0.5f+0.5f) * 0.2f));// +3.0f;
-	}
 }
---
+++ Assets/Scripts/ProjectileSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProjectileSpawner : MonoBehaviour {
+
+	// Allow adjustment of GameObjects (the projectiles) and the fire rate (stored in seconds) through the editor
+	public List<GameObject> projectiles = new List<GameObject>();
+	public float fireRate = 2F;
+	public float velocity = 450F;
+
+	private float nextFire = 0.0F;
+	private int randomObjectIndex;
+
+	// Use FixedUpdate because there are physics calculations here
+	void FixedUpdate() {
+
+		// If the current time is greater than the next fire time, then throw another projectile at the player
+		if (Time.time > nextFire)
+			FireProjectile();
+
+	}
+
+	void FireProjectile() {
+
+		// Next fire is the current time plus the fire rate (in seconds)
+		nextFire = Time.time + fireRate;
+
+		// Pick a random projectile from the list and create it
+		randomObjectIndex = Random.Range(0, projectiles.Count);
+		GameObject projectile = Instantiate(projectiles[randomObjectIndex], transform.position, transform.rotation) as GameObject;
+
+		// Add some force and some spin to the projectile
+		projectile.rigidbody.AddForce(transform.up * velocity);
+		projectile.rigidbody.AddRelativeTorque(Random.Range(500, 1000),
+		                                       Random.Range(500, 1000),
+		                                       Random.Range(500, 1000));
+
+	}
+	
+}
