688368530bf1e9e70c0c08152b6610ee4ac41a88
Nick Pettit <nick@nickpettit.com>
2015-01-25 11:35:17 -0500

    Added procedural crowd generation

---
+++ Assets/Scripts/CrowdController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrowdController : MonoBehaviour {
+
+	public GameObject reporterPrefab;
+
+	private List<GameObject> crowd = new List<GameObject>();
+	private Animator anim;
+	private float xSpacing = 1.089F;
+	private float zSpacing = 1.381F;
+
+	// Use this for initialization
+	void Awake () {
+
+		// Generate the proceedural crowd
+		for (int z = 0; z < 4; z++) {
+			for (int x = 0; x < 7; x++) {
+				GameObject crowdMember = Instantiate(reporterPrefab, reporterPrefab.transform.position, reporterPrefab.transform.rotation) as GameObject;
+				crowdMember.transform.position = new Vector3(crowdMember.transform.position.x - xSpacing * x,
+				                                             crowdMember.transform.position.y,
+				                                             crowdMember.transform.position.z + zSpacing * z);
+
+				// Add each crowd member to the crowd list
+				crowd.Add(crowdMember);
+
+			}
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/ReporterScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReporterScript : MonoBehaviour {
+
+	Animator anim;
+	int talkHash = Animator.StringToHash("Talk");
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			anim.SetTrigger(talkHash);
+		}
+	}
+}
